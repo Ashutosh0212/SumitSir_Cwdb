b23bf2f8d9c28574238e2af1613c93beec751c30
Devansh Kaushik <devansh007kaushik@gmail.com>
2024-02-06 14:31:17 +0530

    backup script change

--- cwdb_website/scripts/backup_script.bat
+++ cwdb_website/scripts/backup_script.bat
@@ -1,6 +1,13 @@
 @echo off
 
-set /p password=Enter password: 
+rem Check if the number of arguments is less than 1
+if "%~1"=="" (
+    echo Error: Password argument is missing.
+    exit /b 1
+)
+
+rem Set the password from the first argument
+set password=%~1
 
 rem Check if the entered password matches the expected password
 if "%password%"=="Cwdb_MIS_valkyron_77" (
@@ -8,4 +15,5 @@ if "%password%"=="Cwdb_MIS_valkyron_77" (
     python C:\path\to\your\manage.py backup_data
 ) else (
     echo Incorrect password. Backup operation aborted.
+    exit /b 1
 )
