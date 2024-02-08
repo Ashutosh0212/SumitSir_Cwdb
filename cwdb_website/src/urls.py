b336a6f84a213c5b346bcd281a8d6f8921dd7a3c
dasanivishu <vishaldasani1999@gmail.com>
2024-02-08 14:36:24 +0530

    requirements.txt

--- cwdb_website/src/urls.py
+++ cwdb_website/src/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from django.urls import include, path
 from django.contrib.auth.views import (
     LoginView,
@@ -38,4 +40,11 @@ urlpatterns = [
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+     path('change-password/', auth_views.PasswordChangeView.as_view(
+        template_name='registration/password_change_form.html',
+        success_url='password_change_done'
+    ), name='change_password'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='registration/password_change_done.html'
+    ), name='password_change_done'),
 ]
