40a35e6360d77bb50f7b1124121316d7926da55e
dasanivishu <vishaldasani1999@gmail.com>
2024-04-29 06:45:41 +0530

    cling

--- cwdb_website/src/wsgi.py
+++ cwdb_website/src/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
+from dj_static import Cling
+
 application = get_wsgi_application()
+application = Cling(application)
+
