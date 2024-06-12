d301e5a35c0e5163a7e0c56dd5b8099216a874b9
dasanivishu <vishaldasani1999@gmail.com>
2024-06-12 15:16:48 +0530

    addded mediacling

--- cwdb_website/src/wsgi.py
+++ cwdb_website/src/wsgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 from dj_static import Cling
 
 application = get_wsgi_application()
-application = Cling(application)
+application =  Cling(MediaCling(application))
 
